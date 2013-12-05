e63614414dcf2e6cdeced3ee386689c0880d463b
siraleem <siraleem@yahoo.com>
2013-12-05 20:00:29 +0500

    XYZ

--- app/scripts/controllers/NavbarController.js
+++ app/scripts/controllers/NavbarController.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('testTakerApp')
-  .controller('NavbarcontrollerCtrl', function ($scope, $location) {
+    .controller('NavbarcontrollerCtrl', function ($scope, $location) {
         $scope.getClass = function (path) {
-            if ($location.path().substr(0, path.length) == path) {
-                return true
-            } else {
-                return false;
-            }
+            /*if ($location.path().substr(0, path.length) == path) {
+             return true
+             } else {
+             return false;
+             }*/
+            return $location.path() == path;
         }
-  });
+    });
